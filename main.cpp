28058e9ddd1141f9fc76e850017fa80ec1c3223b
Alexey Saprunov <workernight@yandex.ru>
2022-06-20 14:53:41 +0300

    two sums

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::vector<int> solution;
+    int size = nums.size();
+    for(int i = 0; i < size; i++) {
+        for (int d=i+1; d < size; d++) {
+            if(nums[i] + nums[d] == target) {
+                solution.push_back(i);
+                solution.push_back(d);
+                return solution;
+            }
+        }
+    }
+    return solution;
+    }
+};
 
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    Solution sol;
+    std::vector<int> v1 = {2,7,11,15};
+    sol.twoSum(v1, 9);
     return 0;
 }
