6102f5463547d93f3522c20a1923c05963807f46
Alexey Saprunov <workernight@yandex.ru>
2022-06-25 13:21:00 +0300

    ransom note

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <map>
 
+class Solution {
+public:
+        bool canConstruct(std::string ransomNote, std::string magazine) {
+            std::map<char, int> map;
+            int mSize = magazine.size();
+            for(int i = 0; i < mSize; i++) {
+                map[magazine[i]]++;
+            }
+            int rSize = ransomNote.size();
+            for(int i = 0; i < rSize; i++) {
+                auto it = map.find(ransomNote[i]);
+                if(it == map.end() ) {
+                    return false;
+                } else {
+                    if(it->second == 0) {
+                        return false;
+                    } else {
+                        map[ransomNote[i]] = it->second-1;
+                    }
+                }
+            }
+
+            return true;
+        }
+};
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    Solution sol;
+    std::cout << sol.canConstruct("abbvve", "abbvgg") << std::endl;
     return 0;
 }
