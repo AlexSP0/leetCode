445427b485942fdfa1919054cee9eb4de5f5b329
Alexey Saprunov <workernight@yandex.ru>
2022-06-25 12:05:11 +0300

    first unique character in the string

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <map>
+#include <unordered_map>
 
+class Solution {
+    std::map<char, int> map;
+public:
+    int firstUniqChar(std::string s) {
+        std::map<char, int> map;
+        for(int i = 0; i < s.size(); i++) {
+            map[s[i]]++;
+        }
+        for(int i = 0; i < s.size(); i++) {
+            if(map[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    Solution sol;
+    std::cout << sol.firstUniqChar("leetcode") << std::endl;
     return 0;
 }
