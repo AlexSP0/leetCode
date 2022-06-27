62d26b34f9bfb08e917a968e574be35605a31264
Alexey Saprunov <workernight@yandex.ru>
2022-06-27 14:37:51 +0400

    valid anagram

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <map>
 
+class Solution {
+    std::map<char, int> mapS;
+public:
+    bool isAnagram(std::string s, std::string t) {
+        mapS.clear();
+        if(s.size() != t.size()) {
+            return false;
+        }
+        for(int i = 0; i < s.size(); i++) {
+            mapS[s[i]]++;
+        }
+        for(int i = 0; i < s.size(); i++) {
+            auto it = mapS.find(t[i]);
+            if(it == mapS.end()) {
+                return false;
+            } else {
+                if(it -> second == 0) {
+                    return false;
+                } else {
+                    mapS[t[i]]--;
+                }
+            }
+        }
+        return true;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    Solution sol;
+    std::cout << sol.isAnagram("rat", "car") << std::endl;
     return 0;
 }
