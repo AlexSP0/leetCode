237ed491073cc570b78247e80c2c3d764cc4b068
Alexey Saprunov <workernight@yandex.ru>
2022-07-06 16:55:23 +0400

    fix: fix memory leaks, algorithm

--- main.cpp
+++ main.cpp
@@ -1,60 +1,92 @@
 #include <iostream>
 #include <vector>
 
-struct ListNode {
+#include <bits/unique_ptr.h>
+
+struct ListNode
+{
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode()
+        : val(0)
+        , next(nullptr)
+    {}
+    ListNode(int x)
+        : val(x)
+        , next(nullptr)
+    {}
+    ListNode(int x, ListNode *next)
+        : val(x)
+        , next(next)
+    {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* reverseList(ListNode* head) {
-        if(head == NULL) return head;
-        if(head->next == NULL) return head;
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == NULL)
+        {
+            return head;
+        }
 
-        ListNode *currentNode = head->next;
+        if (head->next == NULL)
+        {
+            return head;
+        }
+
+        ListNode *currentNode  = head->next;
         ListNode *previousNode = head;
-        ListNode *nextNode = currentNode->next;
-        previousNode->next = NULL;
-        currentNode->next = previousNode;
-        if(nextNode == NULL) {
-            return currentNode;
-        }
-        previousNode->next = NULL;
-        currentNode->next = previousNode;
-        while(nextNode != NULL) {
-            previousNode = currentNode;
-            currentNode = nextNode;
-            nextNode = nextNode->next;
+        previousNode->next     = NULL;
 
-            currentNode->next = previousNode;
-            if(nextNode == NULL) {
-                currentNode->next = previousNode;
-                return currentNode;
+        while (currentNode != NULL)
+        {
+            ListNode *nextNode = currentNode->next;
+            currentNode->next  = previousNode;
+
+            if (nextNode == NULL)
+            {
+                break;
             }
+
+            previousNode = currentNode;
+            currentNode  = nextNode;
         }
+
         return currentNode;
     }
 
-    ListNode* generateList(std::vector<int> v) {
-        ListNode *head = new ListNode(v.at(0));
+    ListNode *generateList(std::vector<int> v)
+    {
+        auto firstElement     = std::make_unique<ListNode>(v.at(0));
+        ListNode *head        = firstElement.get();
         ListNode *currentNode = head;
-        for(int i = 1; i < v.size(); i++) {
-            ListNode *newNode = new ListNode(v.at(i));
-            currentNode->next = newNode;
-            currentNode = newNode;
+
+        nodes.push_back(std::move(firstElement));
+
+        for (size_t i = 1; i < v.size(); i++)
+        {
+            auto lastElement = std::make_unique<ListNode>(v.at(i));
+
+            currentNode->next = lastElement.get();
+            currentNode       = lastElement.get();
+
+            nodes.push_back(std::move(lastElement));
         }
         return head;
     }
+
+private:
+    std::vector<std::unique_ptr<ListNode>> nodes;
 };
+
 int main()
 {
     Solution sol;
-    ListNode *list1 = sol.generateList({1,2,6,3,4,5,6});
-    ListNode *list2 = sol.reverseList(list1);
+    ListNode *list1 = sol.generateList({1, 2, 6, 3, 4, 5, 6});
+    sol.reverseList(list1);
+
     std::cout << "Hello world!" << std::endl;
     return 0;
 }
