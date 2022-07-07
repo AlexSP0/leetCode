3501fe1ed5d233beae86746a0cae8e9c8db08486
Alexey Saprunov <workernight@yandex.ru>
2022-07-07 11:31:28 +0400

    valid parentheses

--- main.cpp
+++ main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
+#include <stack>
+#include <string>
 
+class Solution
+{
+public:
+    bool isValid(std::string s)
+    {
+        std::stack<char> st;
+        for (char i : s)
+        {
+            if (i == '(' || i == '{' || i == '[')
+            {
+                st.push(i);
+            }
+            else if (i == ')' && st.size() && st.top() == '(')
+            {
+                st.pop();
+            }
+            else if (i == '}' && st.size() && st.top() == '{')
+            {
+                st.pop();
+            }
+            else if (i == ']' && st.size() && st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};
 
 int main()
 {
