220766bc7d0709f6adc075611315d4df213e0d7c
Alexey Saprunov <workernight@yandex.ru>
2022-07-11 22:16:09 +0400

    search in a binary search tree

--- main.cpp
+++ main.cpp
@@ -1,8 +1,90 @@
 #include <iostream>
+#include <memory>
+#include <vector>
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr)
+    {}
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr)
+    {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x)
+        , left(left)
+        , right(right)
+    {}
+};
+
+class Solution
+{
+private:
+    inline static int currentIndex;
+    std::vector<std::unique_ptr<TreeNode>> mNodes;
+
+public:
+    Solution() { Solution::currentIndex = 0; }
+
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (root == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (root->left == nullptr && root->right == nullptr && root->val == val)
+        {
+            return root;
+        }
+
+        if (root->val == val)
+        {
+            return root;
+        }
+        if (root->val > val)
+        {
+            return searchBST(root->left, val);
+        }
+        else
+        {
+            return searchBST(root->right, val);
+        }
+    }
+
+    TreeNode *buildTree(std::vector<int> values)
+    {
+        TreeNode *newNode = nullptr;
+        if (currentIndex < values.size() && values[currentIndex] < 101
+            && values[currentIndex] > -101)
+        {
+            mNodes.push_back(
+                std::make_unique<TreeNode>(TreeNode(values[currentIndex], nullptr, nullptr)));
+            newNode = mNodes.back().get();
+            currentIndex++;
+            newNode->left  = buildTree(values);
+            newNode->right = buildTree(values);
+        }
+        else
+        {
+            currentIndex++;
+        }
+        return newNode;
+    }
+};
 
 int main()
 {
+    Solution solution;
+    TreeNode *firstNode = solution.buildTree({40, 20, 60, 10, 30, 50, 70});
+    TreeNode *result    = solution.searchBST(firstNode, 20);
     std::cout << "Hello world!" << std::endl;
     return 0;
 }
