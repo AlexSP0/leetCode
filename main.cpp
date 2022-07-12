0c34c1be80ff9f9b6484fa393b99a71cb7463539
Alexey Saprunov <workernight@yandex.ru>
2022-07-12 12:51:30 +0400

    two sums - input BST

--- main.cpp
+++ main.cpp
@@ -1,8 +1,103 @@
 #include <iostream>
+#include <memory>
+#include <vector>
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr)
+    {}
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr)
+    {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x)
+        , left(left)
+        , right(right)
+    {}
+};
+
+class Solution
+{
+private:
+    inline static int currentIndex;
+    std::vector<std::unique_ptr<TreeNode>> mNodes;
+    std::vector<int> values;
+
+public:
+    Solution() { currentIndex = 0; }
+
+    bool findTarget(TreeNode *root, int k)
+    {
+        inorderTraversal(root);
+        int i = 0, j = values.size() - 1;
+        while (i < j)
+        {
+            if (values[i] + values[j] == k)
+            {
+                return 1;
+            }
+            else if (values[i] + values[j] > k)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return 0;
+    }
+
+    void inorderTraversal(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            values.push_back(root->val);
+            return;
+        }
+        inorderTraversal(root->left);
+        values.push_back(root->val);
+        inorderTraversal(root->right);
+        return;
+    }
+
+    TreeNode *buildTree(std::vector<int> values)
+    {
+        TreeNode *newNode = nullptr;
+        if (currentIndex < values.size() && values[currentIndex] < 101
+            && values[currentIndex] > -101)
+        {
+            mNodes.push_back(
+                std::make_unique<TreeNode>(TreeNode(values[currentIndex], nullptr, nullptr)));
+            newNode = mNodes.back().get();
+            currentIndex++;
+            newNode->left  = buildTree(values);
+            newNode->right = buildTree(values);
+        }
+        else
+        {
+            currentIndex++;
+        }
+        return newNode;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    Solution solution;
+    TreeNode *firstNode = solution.buildTree({1, 2, -1000, -1000, 3, 4, -1000, -1000, 5});
+    std::cout << solution.findTarget(firstNode, 3) << std::endl;
     return 0;
 }
